7916e811a53567e88c68117f6d9b5fd4f76c5ead
Kevan <khnguyen381@gmail.com>
2024-10-01 00:53:07 -0700

    chore: remove mantine packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { MantineProvider } from '@mantine/core';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './api/queryClient';
 import AppRouter from './routes/AppRouter';
 
 export const App = () => {
     return (
-        <MantineProvider>
-            <QueryClientProvider client={queryClient}>
-                <AppRouter />
-            </QueryClientProvider>
-        </MantineProvider>
+        <QueryClientProvider client={queryClient}>
+            <AppRouter />
+        </QueryClientProvider>
     );
 };
 
