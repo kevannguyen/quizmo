0f1bcf2d5ed74dd90638a8481512d09706cc1cb5
Kevan <khnguyen381@gmail.com>
2024-10-06 22:02:07 -0700

    feat: hook up cards to jotai

--- src/pages/CollectionQuiz/CollectionQuiz.tsx
+++ src/pages/CollectionQuiz/CollectionQuiz.tsx
@@ -7,8 +7,23 @@ import {
     QuizQuestion,
     QuizResponse,
 } from '@/api/services/collectionService/interfaces';
-import { memo } from 'react';
+import { memo, useEffect, useMemo } from 'react';
 import { cn } from '@/lib/utils';
+import { atom, useAtom } from 'jotai';
+import _ from 'lodash';
+
+const selectedResponsesAtom = atom<(number | null)[] | null>(null);
+
+// Shuffles list in-place
+const shuffleList = (items: unknown[]) => {
+    // Fisher-Yates Shuffle Algorithm
+    for (let i = items.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+
+        // Swap the current item with the random item
+        [items[i], items[randomIndex]] = [items[randomIndex], items[i]];
+    }
+};
 
 export const CollectionQuiz = () => {
     const { collectionId = '' } = useParams();
@@ -22,20 +37,58 @@ export const CollectionQuiz = () => {
         enabled: !!collectionId,
         retry: 0,
     });
+    const [selectedResponses, setSelectedResponses] = useAtom(
+        selectedResponsesAtom
+    );
+    const shuffledQuestions = useMemo(() => {
+        if (!collectionData || collectionData?.questions?.length === 0) {
+            return null;
+        }
 
-    const questions = collectionData?.questions ?? [];
+        const questions = _.cloneDeep(collectionData.questions);
+        shuffleList(questions);
+        for (let i = 0; i < questions.length; i += 1) {
+            shuffleList(questions[i].responses);
+        }
 
-    console.log('lolol collectionData = ', collectionData);
+        return questions;
+    }, [JSON.stringify(collectionData)]);
+
+    const isQuizFinished =
+        (selectedResponses ?? [null]).filter((response) => response === null)
+            .length === 0;
+
+    useEffect(() => {
+        if (!collectionData || collectionData?.questions?.length === 0) {
+            setSelectedResponses(null);
+            return;
+        }
+
+        // Initialize quiz to no selected responses
+        const selectedResponses = [];
+        for (let i = 0; i < collectionData.questions.length; i += 1) {
+            selectedResponses.push(null);
+        }
+
+        setSelectedResponses(selectedResponses);
+
+        console.log('lolol shuffledQuestions = ', shuffledQuestions);
+
+        return () => {
+            setSelectedResponses(null);
+        };
+    }, [JSON.stringify(shuffledQuestions)]);
 
     return (
         <div className="flex flex-col gap-8">
             {collectionIsError && <div>Error loading page</div>}
-            {collectionData &&
-                questions.map(({ question, responses }, index) => (
+            {shuffledQuestions &&
+                shuffledQuestions.map(({ question, responses }, index) => (
                     <QuizCard
                         key={`${question}-${index}`}
                         question={question}
                         responses={responses}
+                        questionIndex={index}
                     ></QuizCard>
                 ))}
         </div>
@@ -43,7 +96,11 @@ export const CollectionQuiz = () => {
 };
 
 const QuizCard = memo(
-    ({ question, responses }: QuizQuestion) => {
+    ({
+        question,
+        responses,
+        questionIndex,
+    }: QuizQuestion & { questionIndex: number }) => {
         return (
             <div tabIndex={-1}>
                 <article className="shadow-md bg-card rounded flex flex-col justify-between m-0 mx-auto max-w-[52.5rem] min-h-[25.75rem] py-6 px-8 transition-all duration-500">
@@ -55,13 +112,17 @@ const QuizCard = memo(
                             Select the correct answer
                         </section>
                         <div className="flex flex-col w-full gap-6 sm:grid sm:grid-cols-2 sm:auto-rows-fr">
-                            {responses.map(({ response, correct }) => (
-                                <QuizResponseOption
-                                    key={response}
-                                    response={response}
-                                    correct={correct}
-                                ></QuizResponseOption>
-                            ))}
+                            {responses.map(
+                                ({ response, correct }, responseIndex) => (
+                                    <QuizResponseOption
+                                        key={response}
+                                        response={response}
+                                        correct={correct}
+                                        questionIndex={questionIndex}
+                                        responseIndex={responseIndex}
+                                    ></QuizResponseOption>
+                                )
+                            )}
                         </div>
                     </div>
                 </article>
@@ -75,15 +136,40 @@ const QuizCard = memo(
 
 const QuizResponseOption = ({
     response,
-    correct,
-    selected,
-}: QuizResponse & { selected?: boolean }) => {
+    questionIndex,
+    responseIndex,
+}: QuizResponse & {
+    questionIndex: number;
+    responseIndex: number;
+}) => {
+    const [selectedResponses, setSelectedResponses] = useAtom(
+        selectedResponsesAtom
+    );
+    const selectedResponseIndex = selectedResponses?.[questionIndex] ?? null;
+
+    const handleSelect = () => {
+        if (!selectedResponses) return;
+
+        const newSelectedResponses = [...selectedResponses];
+        const currentSelection = newSelectedResponses[questionIndex];
+
+        newSelectedResponses[questionIndex] =
+            currentSelection === responseIndex ? null : responseIndex;
+
+        setSelectedResponses(newSelectedResponses);
+    };
+
     return (
         <div
             className={cn(
                 'p-4 rounded border border-input hover:bg-accent hover:text-accent-foreground cursor-pointer flex-grow',
-                { 'bg-blue-100 border-blue-300 hover:bg-blue-100': selected }
+                {
+                    'bg-blue-100 border-blue-300 hover:bg-blue-100':
+                        selectedResponseIndex === responseIndex,
+                }
             )}
+            onClick={handleSelect}
+            tabIndex={0}
         >
             {response}
         </div>
